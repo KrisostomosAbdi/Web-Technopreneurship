6b01949e03e19cf54514a040284e91c17b9c1344
Krisostomos Abdi <105838486+Abdixta-Winarto@users.noreply.github.com>
2022-06-30 14:08:11 +0700

    mengganti logo

--- login1/login.php
+++ login1/login.php
@@ -58,6 +58,8 @@ if (isset($_POST["login"])) {
 	<meta http-equiv='expires' content='0'>
 	<meta http-equiv='pragma' content='no-cache'>
 
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="../assets/img/favicon.png">
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
